f08d6293f43d8b1b3b1585809b039315cf0dcb59
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-04 02:11:40 +0530

    implement self-destruct pattern the right way

--- self-destruct-pattern.sol
+++ self-destruct-pattern.sol
@@ -2,26 +2,38 @@
 pragma solidity >=0.5.0 <0.9.0;
 contract selfDestruct{
     address payable public manager;
-    
+    bool destroyed;
+
     constructor() {
         manager = payable(msg.sender);
     }
 
-    function sendEtherContract() public payable {
+    modifier isNotDestroyed{
+        require(destroyed!=true,"The contract has been destroyed!");
+        _;
+    }
+
+    function sendEtherContract() public payable isNotDestroyed {
 
     }
 
-    function contractBalance() public view returns(uint) {
+    function contractBalance() public view isNotDestroyed returns(uint) {
         return address(this).balance;
     }
 
-    receive() external payable {
+    receive() external payable isNotDestroyed {
 
     } 
 
-    function destroy() public  {
+    // function destroy() public  {
+    //     require(manager==msg.sender,"You are not the manager!");
+    //     selfdestruct(manager); Bad pratice
+    // }   
+
+    function destroy() public isNotDestroyed {
         require(manager==msg.sender,"You are not the manager!");
-        selfdestruct(manager);
-    }   
+        manager.transfer(contractBalance());
+        destroyed = true;
+    }
 
 }
