320d9741a4cdd6b2131f5c103e4101e937f4ede8
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-04 02:27:07 +0530

    withdrawl pattern - the right way

--- withdrawl-pattern.sol
+++ withdrawl-pattern.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0 <0.9.0;
 contract withdrawl{
     address payable public richest;
     uint max;
-
+    mapping(address => uint) public richAddress;
     constructor() payable{
         richest = payable (msg.sender);
         max = msg.value;
@@ -14,8 +14,17 @@ contract withdrawl{
         require(msg.value>max,"You are not the richest person!");
         richest = payable (msg.sender);
         max = msg.value;
-        richest.transfer(max);
+        richAddress[richest] = max;
+        // richest.transfer(max);
     }
+
+    //Send and receive functions should be different.
+    function withdraw() external payable {
+        uint amount = richAddress[msg.sender];
+        richAddress[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
+
 }
 
 contract player{
